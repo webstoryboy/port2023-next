b3ec3c9859761773c53ffce9b3fe810c3300a553
webstoryboy <webstupids@gmail.com>
2023-08-05 18:09:28 +0900

    😛 연락처 완료?

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+
+import { contactText } from "@/constants";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+    return (
+        <section id="contact">
+            <div className="contact__inner">
+                <h2 className="contact__title">Contact</h2>
+                <div className="contact__lines top" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+                <div className="contact__text">
+                    <div className="text">
+                        {contactText.map((contact, key) => (
+                            <div key={key}>
+                                <a href={contact.link} target="_blank">{contact.title}</a>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="contact__lines" aria-hidden="true">
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Contact
--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -6,20 +6,20 @@ import { portText } from "@/constants";
 const Port = () => {
     return (
         <section id="port">
-            <div class="port__inner">
-                <h2 class="port__title">
+            <div className="port__inner">
+                <h2 className="port__title">
                     portfolio <em>포폴 작업물</em>
                 </h2>
-                <div class="port__wrap">
+                <div className="port__wrap">
                     {portText.map((port, key) => (
-                        <article class={`port__item p${key+1}`} key={key}>
-                            <span class="num">{port.num}.</span>
-                            <a href={port.code} target="_blank" class="img">
+                        <article className={`port__item p${key+1}`} key={key}>
+                            <span className="num">{port.num}.</span>
+                            <a href={port.code} target="_blank" className="img">
                                 <Image src={port.img} alt={port.name} width={420} height={262} />
                             </a>
-                            <h3 class="title">{port.title}</h3>
-                            <p class="desc">{port.desc}</p>
-                            <a href={port.view} target="_blank" class="site">사이트 보기</a>
+                            <h3 className="title">{port.title}</h3>
+                            <p className="desc">{port.desc}</p>
+                            <a href={port.view} target="_blank" className="site">사이트 보기</a>
                         </article>
                     ))}
                 </div>
@@ -28,4 +28,4 @@ const Port = () => {
     )
 }
 
-export default Port
+export default Port;
