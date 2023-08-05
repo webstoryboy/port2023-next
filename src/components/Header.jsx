3f85b56c4bc44b061dad1c222f0c41564839f881
webstoryboy <webstupids@gmail.com>
2023-08-05 16:52:12 +0900

    😂 헤더 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, {useState} from "react";
+
+import { headerNav } from "@/constants";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    const [show, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    }
+
+    return (
+        <header id="header" role="banner">
+            <div className="header__inner">
+                <div className="header__logo">
+                    <h1>
+                        <a href="#">portfolio<em>next.js</em></a>
+                    </h1>
+                </div>
+                <nav 
+                    className={`header__nav ${show ? "show" : ""}`} 
+                    role="navigation" 
+                    aria-label="메인 메뉴"
+                >
+                    <ul>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+                <div 
+                    className="header__nav__mobile" 
+                    id="headerToggle" 
+                    aria-controls="primary-menu" 
+                    aria-expanded={show ? "true" : "false"} 
+                    role="button"
+                    tabIndex="0"
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                </div>
+            </div>
+        </header>
+    )
 }
 
 export default Header
--- src/components/Skip.jsx
+++ src/components/Skip.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Skip = () => {
-  return (
-    <div>Skip</div>
-  )
-}
+    return (
+        <div id="skip">
+            <a href="#header">헤더 영역 바로가기</a>
+            <a href="#intro">소개 영역 바로가기</a>
+            <a href="#skill">스킬 영역 바로가기</a>
+            <a href="#site">사이트 영역 바로가기</a>
+            <a href="#port">포트폴리오 영역 바로가기</a>
+            <a href="#contact">연락처 영역 바로가기</a>
+            <a href="#footer">푸터 영역 바로가기</a>
+        </div>
+    )
+} 
 
-export default Skip
+export default Skip;
